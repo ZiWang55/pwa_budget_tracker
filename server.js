8ab24467213fc54e0465fc4c42ad72499471ebaa
zi wang <ziwang55@gmail.com>
2021-02-23 09:19:23 -0600

    changing mongodb_uri

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
 const app = express();
 
 app.use(logger("dev"));
@@ -15,13 +17,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
+  useFindAndModify: false
 });
-
 // routes
 app.use(require("./routes/api.js"));
 
