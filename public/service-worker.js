05ea02a5c21076f67253cc218a065c5483e1d5e1
zi wang <ziwang55@gmail.com>
2021-02-23 12:43:12 -0600

    fixing server-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,12 @@
-
 const FILES_TO_CACHE = [
-  './index.html',
-  './css/styles.css',
-  './index.js',
-  './db.js',
-  './manifest.json',
-  './icons/icon-512x512.png',
-  './icons/icon-384x384.png',
-  './icons/icon-192x192.png',
-  './icons/icon-152x152.png',
-  './icons/icon-144x144.png',
-  './icons/icon-128x128.png',
-  './icons/icon-96x96.png',
-  './icons/icon-72x72.png'
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/index.js",
+  "/db.js",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -81,3 +75,4 @@ self.addEventListener("fetch", function(evt) {
     })
   );
 });
+
