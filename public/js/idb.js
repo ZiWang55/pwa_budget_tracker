9fb44901ff7603428cd407790267d6d9c230d04a
zi wang <ziwang55@gmail.com>
2021-02-22 22:09:05 -0600

    adding db uploadTransaction function

--- public/js/idb.js
+++ public/js/idb.js
@@ -17,4 +17,43 @@ request.onerror = function(event) {
     console.log(event.taget.errorCode)
 }
 
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite')
+    const budgetObjectStore = transaction.objectStore('new_transaction')
+    budgetObjectStore.add(record)
+}
+
+function uploadTransction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite')
+    const budgetObjectStore = transaction.objectStore('new_transaction')
+    const getAll = budgetObjectStore.getAll();
+  
+    getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          const budgetObjectStore = transaction.objectStore('new_transaction');
+          budgetObjectStore.clear();
+
+          alert('All saved transactions has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
 
