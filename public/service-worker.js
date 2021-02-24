f2048ec5c0125823c2202e08a1e579ffd4e0240b
zi wang <ziwang55@gmail.com>
2021-02-23 18:22:24 -0600

    test

--- public/service-worker.js
+++ public/service-worker.js
@@ -46,6 +46,7 @@ self.addEventListener("activate", function(evt) {
 self.addEventListener("fetch", function(evt) {
   // cache successful requests to the API
   if (evt.request.url.includes("/api/transaction")) {
+    console.log(evt, "test")
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
         return fetch(evt.request)
